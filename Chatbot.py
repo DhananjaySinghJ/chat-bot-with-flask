04e79f5b25c39beecca3ec93145cc5e65c279427
dhananjay-prodt <dhananjay@prodt.co>
2024-07-25 10:56:13 +0530

    "Added index.html files"

--- Chatbot.py
+++ Chatbot.py
@@ -1,95 +1,111 @@
-import nltk
-from nltk.corpus import movie_dialogs
+import os
 import numpy as np
 import pandas as pd
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Embedding, LSTM, Dense
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from flask import Flask, render_template, request
 
-# Download NLTK data
-nltk.download('punkt')
-nltk.download('stopwords')
+# Parameters
+max_length = 20
+vocab_size = 10000
+
+# Load and preprocess the data
+def load_conversations():
+    lines = open('cornell movie-dialogs corpus/movie_lines.txt', encoding='utf-8', errors='ignore').read().split('\n')
+    conversations = open('cornell movie-dialogs corpus/movie_conversations.txt', encoding='utf-8', errors='ignore').read().split('\n')
+    
+    id2line = {}
+    for line in lines:
+        _line = line.split(' +++$+++ ')
+        if len(_line) == 5:
+            id2line[_line[0]] = _line[4]
+
+    conversations_ids = []
+    for conversation in conversations[:-1]:
+        _conversation = conversation.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
+        conversations_ids.append(_conversation.split(','))
+
+    questions = []
+    answers = []
+
+    for conversation in conversations_ids:
+        for i in range(len(conversation) - 1):
+            questions.append(id2line[conversation[i]])
+            answers.append(id2line[conversation[i + 1]])
+
+    return questions, answers
+
+questions, answers = load_conversations()
 
 def preprocess_text(text):
-    # Tokenization
-    tokens = nltk.word_tokenize(text.lower())
-
-    # Remove stop words
-    stop_words = set(nltk.corpus.stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
-
+    tokens = word_tokenize(text.lower())
+    stop_words = set(stopwords.words('english'))
+    tokens = [word for word in tokens if word not in stop_words and word.isalnum()]
     return tokens
 
-# Load movie dialogs
-lines = movie_dialogs.lines()
+questions = [' '.join(preprocess_text(question)) for question in questions]
+answers = [' '.join(preprocess_text(answer)) for answer in answers]
 
-# Create a DataFrame
-conversations = movie_dialogs.conversations()
-df = pd.DataFrame({'lineID': [line[0] for line in lines],
-                   'characterID': [line[1] for line in lines],
-                   'movieID': [line[2] for line in lines],
-                   'character': [line[3] for line in lines],
-                   'text': [line[4] for line in lines]})
+tokenizer = Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(questions + answers)
 
-# Merge with conversations to get conversation context
-df['conversationID'] = [conv[0] for conv in conversations]
-df['utterance_number'] = [conv.index(lineID) for conv, lineID in zip(conversations, df['lineID'])]
+questions_seq = tokenizer.texts_to_sequences(questions)
+answers_seq = tokenizer.texts_to_sequences(answers)
 
-# Group by conversation and create a new column with concatenated utterances
-df_grouped = df.groupby('conversationID')['text'].apply(lambda x: ' '.join(x)).reset_index()
+max_length = max([len(seq) for seq in questions_seq + answers_seq])
+questions_padded = pad_sequences(questions_seq, maxlen=max_length, padding='post')
+answers_padded = pad_sequences(answers_seq, maxlen=max_length, padding='post')
 
-# Tokenization and padding
-tokenizer = Tokenizer(num_words=10000)
-tokenizer.fit_on_texts(df_grouped['text'])
-
-sequences = tokenizer.texts_to_sequences(df_grouped['text'])
-max_length = max(len(seq) for seq in sequences)
-padded_sequences = pad_sequences(sequences, maxlen=max_length)
-
-# Create input and output sequences
-X, y = padded_sequences[:-1], padded_sequences[1:]
+X = questions_padded
+y = answers_padded
 
 # Create the model
 model = Sequential()
-model.add(Embedding(10000, 64, input_length=max_length))
+model.add(Embedding(vocab_size, 64, input_length=max_length))
+model.add(LSTM(64, return_sequences=True))
+model.add(Dropout(0.5))
 model.add(LSTM(64))
-model.add(Dense(10000, activation='softmax'))
+model.add(Dense(vocab_size, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Train the model
 model.fit(X, y, epochs=10, batch_size=64)
 
-def chatbot_response(user_input):
-    # Preprocess user input
-    user_input_seq = tokenizer.texts_to_sequences([user_input])
-    user_input_padded = pad_sequences(user_input_seq, maxlen=max_length)
+def generate_response(model, tokenizer, input_text, max_length):
+    input_seq = tokenizer.texts_to_sequences([input_text])
+    input_padded = pad_sequences(input_seq, maxlen=max_length, padding='post')
+    predicted_seq = model.predict(input_padded, verbose=0)
+    predicted_word_index = np.argmax(predicted_seq)
+    predicted_word = tokenizer.index_word.get(predicted_word_index, '')
 
-    # Generate response
-    predicted_index = np.argmax(model.predict(user_input_padded))
-    predicted_word = tokenizer.index_word[predicted_index]
+    response = predicted_word
+    for _ in range(10):
+        input_seq = tokenizer.texts_to_sequences([response])
+        input_padded = pad_sequences(input_seq, maxlen=max_length, padding='post')
+        predicted_seq = model.predict(input_padded, verbose=0)
+        predicted_word_index = np.argmax(predicted_seq)
+        predicted_word = tokenizer.index_word.get(predicted_word_index, '')
+        response += ' ' + predicted_word
 
-    # Generate more words (iterative approach)
-    for _ in range(10):  # Adjust the number of words to generate
-        input_seq = np.append(user_input_padded[0][1:], predicted_index)
-        input_seq = np.reshape(input_seq, (1, max_length))
-        predicted_index = np.argmax(model.predict(input_seq))
-        predicted_word += ' ' + tokenizer.index_word[predicted_index]
+    return response
 
-    return predicted_word
-
+# Flask app
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('chatbot_index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
     user_message = request.form['user_message']
-    chatbot_response = chatbot_response(user_message)
-    return chatbot_response
+    response = generate_response(model, tokenizer, user_message, max_length)
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Image captioning.py
+++ Image captioning.py
@@ -1,24 +1,12 @@
 import os
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
+import cv2
+from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Dropout, add
-from nltk.translate.bleu_score import corpus_bleu
-from PIL import Image
-import cv2
-import nltk
-from nltk.corpus import stopwords
-
-# For Flask
-from flask import Flask, render_template, request, jsonify
-
-# Download NLTK data
-nltk.download('stopwords')
+from tensorflow.keras.layers import Input, LSTM, Embedding, Dense, Dropout
+from flask import Flask, render_template, request
 
 # Parameters
 img_size = (224, 224)
@@ -27,116 +15,16 @@ max_length = 32
 embedding_dim = 256
 lstm_units = 256
 
-def load_images(dir_path):
-    images = []
-    for img_name in os.listdir(dir_path):
-        img_path = os.path.join(dir_path, img_name)
-        img = cv2.imread(img_path)
-        img = cv2.resize(img, img_size)
-        img = img.astype(np.float32) / 255.0
-        images.append(img)
-    return np.array(images)
-
-def load_captions(filename):
-    with open(filename, 'r') as f:
-        doc = f.read()
-
-    descriptions = dict()
-    for line in doc.split('\n'):
-        tokens = line.split()
-        image_id, image_desc = tokens[0], tokens[1:]
-        image_id = image_id.split('.')[0]
-        image_desc = ' '.join(image_desc)
-        if image_id not in descriptions:
-            descriptions[image_id] = list()
-        descriptions[image_id].append(image_desc)
-    return descriptions
-
-def clean_descriptions(descriptions):
-    all_desc = list()
-    for key in descriptions.keys():
-        [all_desc.append(d) for d in descriptions[key]]
-
-    tokenizer = Tokenizer(num_words=vocab_size)
-    tokenizer.fit_on_texts(all_desc)
-    return tokenizer
-
-def preprocess_captions(descriptions, tokenizer, max_length):
-    vocab_size = len(tokenizer.word_index) + 1
-    X = tokenizer.texts_to_sequences(all_desc)
-    X = pad_sequences(X, maxlen=max_length, padding='post')
-    return X, vocab_size
-
-# Load data
-train_images = load_images('flickr8k/images')
-train_descriptions = load_captions('flickr8k/text/Flickr8k.token.txt')
-tokenizer = clean_descriptions(train_descriptions)
-X, vocab_size = preprocess_captions(train_descriptions, tokenizer, max_length)
-
-# Create a mapping from image id to index
-train_ids = list(train_descriptions.keys())
-
 # Load the pre-trained InceptionV3 model
 model = InceptionV3(weights='imagenet', include_top=False)
 
-# Extract features from images
-def extract_features(imgs):
-    features = model.predict(imgs, verbose=1)
-    return features
-
-# Extract features for training images
-train_features = extract_features(train_images)
-
-# Create training and validation sets
-from sklearn.model_selection import train_test_split
-
-X_train, X_val, y_train, y_val, train_ids_train, train_ids_val = train_test_split(train_features, X, train_ids, test_size=0.2, random_state=42)
-
-def create_model(vocab_size, max_length):
-    # Encoder model
-    input_img = Input(shape=(2048,))
-    fe = Dense(256, activation='relu')(input_img)
-    fe = Dropout(0.2)(fe)
-    fe = Dense(256, activation='relu')(fe)
-
-    # Decoder model
-    decoder_input = Input(shape=(max_length,))
-    dec_emb = Embedding(vocab_size, embedding_dim, mask_zero=True)(decoder_input)
-    dec_lstm1 = LSTM(lstm_units, return_sequences=True)(dec_emb, initial_state=[fe, fe])
-    dec_lstm2 = LSTM(lstm_units)(dec_lstm1)
-    decoder_outputs = Dense(vocab_size, activation='softmax')(dec_lstm2)
-    model = Model(inputs=[input_img, decoder_input], outputs=decoder_outputs)
-    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam')
-    return model
-
-# Create the model
-model = create_model(vocab_size, max_length)
-
-def data_generator(descriptions, photos, tokenizer, max_length, batch_size):
-    while 1:
-        for i in range(len(photos) // batch_size):
-            start, end = i * batch_size, (i + 1) * batch_size
-            X, y = list(), list()
-            for j in range(start, end):
-                photo = photos[j]
-                in_text = descriptions[train_ids[j]]
-                for desc in in_text:
-                    seq = tokenizer.texts_to_sequences([desc])[0]
-                    for i in range(1, len(seq)):
-                        in_seq, out_seq = seq[:i], seq[i]
-                        in_seq = pad_sequences([in_seq], maxlen=max_length)[0]
-                        X.append(photo)
-                        y.append(out_seq)
-            yield np.array(X), np.array(y)
-
-# Train the model
-epochs = 10
-batch_size = 64
-steps_per_epoch = len(X_train) // batch_size
-
-for i in range(epochs):
-    generator = data_generator(train_descriptions, X_train, tokenizer, max_length, batch_size)
-    model.fit(generator, epochs=1, steps_per_epoch=steps_per_epoch, verbose=1)
+# Function to preprocess images
+def preprocess_image_for_inference(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (224, 224))
+    img = img.astype(np.float32) / 255.0
+    img = np.expand_dims(img, axis=0)
+    return preprocess_input(img)
 
 def generate_desc(model, tokenizer, photo, max_length):
     in_text = 'start'
@@ -156,13 +44,6 @@ def generate_desc(model, tokenizer, photo, max_length):
 
 app = Flask(__name__)
 
-def preprocess_image_for_inference(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.resize(img, (224, 224))
-    img = img.astype(np.float32) / 255.0
-    img = np.expand_dims(img, axis=0)
-    return preprocess_input(img)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
@@ -177,8 +58,8 @@ def index():
             img = preprocess_image_for_inference(img_path)
             img_features = model.layers[1].predict(img)
             caption = generate_desc(model, tokenizer, img_features, max_length)
-            return render_template('index.html', caption=caption)
-    return render_template('index.html')
+            return render_template('image_captioning_index.html', caption=caption)
+    return render_template('image_captioning_index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
